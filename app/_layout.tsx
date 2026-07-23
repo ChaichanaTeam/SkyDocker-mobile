fb94098bc424f1fadb0b09176739720da83bb75e
ProbablyNoth1ng <android.kaplan@gmail.com>
2026-07-23 20:41:05 +0200

    fix: connect splashscreen with signin, add svgs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout() {
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack screenOptions={{ headerShown: false }}>
           <Stack.Screen name="index" />
-          <Stack.Screen name="(auth)" />
           <Stack.Screen name="(tabs)" />
           <Stack.Screen
             name="modal"
