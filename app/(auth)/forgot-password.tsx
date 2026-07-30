ae13687544385313bd880db2228229dffb24299d
ProbablyNoth1ng <android.kaplan@gmail.com>
2026-07-30 13:04:53 +0200

    feat: make forgot password page

---
+++ app/(auth)/forgot-password.tsx
@@ -0,0 +1,5 @@
+import { PasswordScreen } from "@/components/ui/auth/signup/PasswordScreen/PasswordScreen";
+
+export default function ForgotPassword() {
+  return <PasswordScreen mode="multiple" variant="forgot-password" />;
+}
