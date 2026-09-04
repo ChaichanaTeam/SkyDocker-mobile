8fcabd5c47d6b9db01010600fadef81812e49132
ProbablyNoth1ng <android.kaplan@gmail.com>
2026-09-04 12:53:56 +0200

    feat: endpoints links update

--- app/api/endpoints-config.ts
+++ app/api/endpoints-config.ts
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  LOGIN: () => "auth/login",
-  SIGNUP: () => "auth/register",
-  VERIFYOTP: () => "auth/register/verify-otp",
-  REQUESTOTP: () => "auth/register/request-otp",
+  LOGIN: () => "api/v1/auth/login",
+  SIGNUP: () => "api/v1/auth/register",
+  VERIFYOTP: () => "api/v1/auth/register/verify-otp",
+  REQUESTOTP: () => "api/v1/auth/register/request-otp",
 } as const;
